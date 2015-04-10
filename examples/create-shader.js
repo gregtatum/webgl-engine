36bc10d5a879c09ec11f54474014f608aba49c52
Greg Tatum <tatum.creative@gmail.com>
2015-04-09 23:19:52 -0500

    Polygons are moving!

--- examples/create-shader.js
+++ examples/create-shader.js
@@ -1,13 +1,80 @@
-var CreateCanvas = require('../lib/canvas')
+var CanvasManager = require('../lib/canvas')
 var ShaderTools = require('../lib/shader')
+var BufferTools = require('../lib/buffers')
 var Fs = require('fs')
+var Camera = require('../lib/camera')
+var Model = require('../lib/model')
+require('../lib/utils/console-tools')
+var CreateLoop = require('poem-loop')
 
-var canvas = CreateCanvas()
-var gl = canvas.gl
 
-var shaders = ShaderTools.bind(gl)
+var canvasManager = CanvasManager()
+var gl = canvasManager.gl
+var shaderTools = ShaderTools.bind(gl)
+var bufferTools = BufferTools.bind(gl)
+var model = Model()
+var camera = Camera( gl )
+var loop = CreateLoop()
 
-var shader = shaders.createProgram(
+// Build shader
+var shaderProgram = shaderTools.createProgram(
 	Fs.readFileSync( __dirname + "/../lib/shaders/model.vert", 'utf8'),
 	Fs.readFileSync( __dirname + "/../lib/shaders/model.frag", 'utf8')
 )
+
+var shader = shaderTools.setup( shaderProgram, {
+	attributes: {
+		position: {
+			buffer: bufferTools.create( new Float32Array([
+				-1,  0, 0,
+				 0, -1, 0,
+				 1,  1, 0,
+				-1,  0, -1,
+				 0, -1, -1,
+				 1,  1, -1,
+				-1,  0, -2,
+				 0, -1, -2,
+				 1,  1, -2
+			])),
+			bufferType: gl.ARRAY_BUFFER,
+			dataType: gl.FLOAT,
+			size: 3
+		},
+		color: {
+			buffer: bufferTools.create( new Float32Array([
+				 1.0,  0.0, 0.0,
+				 1.0,  1.0, 0.0,
+				 1.0,  0.0, 1.0,
+				 0.0,  1.0, 0.0,
+				 1.0,  1.0, 0.0,
+				 0.0,  1.0, 1.0,
+				 0.0,  0.0, 1.0,
+				 1.0,  0.0, 1.0,
+				 0.0,  1.0, 1.0
+			])),
+			bufferType: gl.ARRAY_BUFFER,
+			dataType: gl.FLOAT,
+			size: 3
+		}
+	},
+	uniforms: {
+		projection : {
+			value: camera.projection,
+			type: "Matrix4fv"
+		},
+		modelView : {
+			value: model.modelView,
+			type: "Matrix4fv"
+		}
+	}
+})
+
+loop.emitter.on('update', function() {
+	
+	model.updateModelView( camera.view )
+	shader.bind()
+	gl.drawArrays( gl.TRIANGLES, 0, 9 )
+})
+loop.start()
+
+// debugger
--- lib/shaders/model.vert
+++ lib/shaders/model.vert
@@ -1,10 +1,14 @@
 precision mediump float;
 
 attribute vec3 position;
+attribute vec3 color;
 
 uniform mat4 projection;
 uniform mat4 modelView;
 
+varying vec3 vColor;
+
 void main() {
+	vColor = color;
 	gl_Position = projection * modelView * vec4(position, 1.0);
 }
